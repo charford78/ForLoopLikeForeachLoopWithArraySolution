402f7511df2cf3e8499f5092ff32f5c0ebe29dbf
Charles Ford <charles.ford1978@gmail.com>
2022-01-07 23:21:48 -0500

    Add project files.

---
+++ ForLoopLikeForeachLoopWithArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+foreach(int num in numbers)
+{
+    Console.Write($"{num} ");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{numbers[i]} ");
+}
+
+Console.WriteLine();
+
+
